7ed997e52cd75cf145053a5f841c7cf0bdcb3458
FritzFart <33838870+FritzFart@users.noreply.github.com>
2023-01-07 12:30:25 +0100

    Patch 3 (#12)
    * Update Pavel.py
    
    Neuer Data import für Topspin 4x. Die NMRraw dateien sind anders formatiert.
    
    * Update base.py
    
    * Update Pavel.py
    
    * Update Pavel.py
    
    * Update base.py

--- eNMRpy/Measurement/base.py
+++ eNMRpy/Measurement/base.py
@@ -48,6 +48,21 @@ class Measurement(object):
         # read in the bruker formatted data
         self.dic, self.data = ng.bruker.read(self.dateipfad)
         self.pdata = ng.bruker.read_procs_file(self.dateipfad+"/pdata/1")
+        lst_acqus__coreheader = self.dic['acqus']['_coreheader']
+        self.split = lst_acqus__coreheader[0].split()
+        self.version = int(self.split[4][0])
+        if self.version >3:
+            self.lenarr = len(self.data)/self.dic['proc2s']['TDeff']
+            self.data1 = np.empty((0,int(self.lenarr-1)))
+            for i in range (self.dic['proc2s']['TDeff']):
+                lower = int(i*self.lenarr)
+                upper = int(lower+self.lenarr-1)
+                lst =list(self.data[lower:upper])
+                self.data1 = np.append(self.data1,[lst],axis=0)
+            self.data = self.data1
+        print('The Topspin %i import was used'%self.version)
+        
+       
         # original dataset
         self.data_orig = self.data
 
